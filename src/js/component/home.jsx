81b0d5a040cf23f52e571cd10dd1ffb1f9b1b0b3
Miguel Angel <160284274+Mitronios@users.noreply.github.com>
2024-05-22 01:24:23 +0000

    To Do List with React

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [addTask, setAddTask] = useState("");
+	const [newTask, setNewTask] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<div className="container text-center">
+				<h1>todos</h1>
+			</div>
+			<ul className="container list-group w-50 shadow p-3 mb-5 bg-body rounded">
+				<input type="text"
+					onChange={(event) => setAddTask(event.target.value)}
+					value={addTask}
+					onKeyDown={(event) => {
+						if (event.key === "Enter") {
+							if
+								(addTask.trim() !== "") {
+								setNewTask(newTask.concat([addTask.trim()]));
+								setAddTask("");
+							}
+						}
+					}
+					}
+					className="form-control shadow  bg-body rounded px-5"
+					placeholder="What needs to be done?"
+					aria-label="Username"
+				/>
+				{newTask.map((task, index) => (
+					<li key={index} className="d-flex justify-content-between list-group-item shadow  bg-body rounded px-5">
+						{task} {""}
+						<span><i className="fa-solid fa-x text-danger delete-icon" onClick={() =>
+							setNewTask(newTask.filter(
+								(task, currentIndex) =>
+									index != currentIndex))
+						}></i></span>
+					</li>
+				))}
+				<div className="text-muted my-1">{newTask.length} tasks</div>
+			</ul>
+		</React.Fragment>
 	);
 };
 
